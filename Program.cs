a7ecd9f69cb5c149c473b4b5b5f597152e4139c3
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2025-10-16 12:51:06 +0200

    Change HttpClient base address to localhost
    Updated the HttpClient base address for testing.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//Change the uri and the launch options to localhost for test the app
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.1.231:5001/") });
 
 await builder.Build().RunAsync();
